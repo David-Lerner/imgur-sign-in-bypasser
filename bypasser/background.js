e4105e1f2de6e5ad1460f4956de8aa78b855afe4
David <dale7642@gmail.com>
2020-02-12 19:34:46 -0800

    Initial commit

---
+++ bypasser/background.js
@@ -0,0 +1,21 @@
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+    convertUrl();
+});
+
+// Called when the user uses the keyboard shortcut
+chrome.commands.onCommand.addListener(function(command) {
+    convertUrl();
+});
+
+function convertUrl() {
+    var targ,newTarg; //Declaring variables
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+	targ=tabs[0].url;
+	newTarg=targ+"/embed?pub=true";
+	return newTarg;
+    });
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+	chrome.tabs.update({url:newTarg});
+    });
+}
